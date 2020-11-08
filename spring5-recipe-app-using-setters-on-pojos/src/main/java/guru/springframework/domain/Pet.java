ade56bfc82006b9166a0e76c621b8ecb3eb3597e
rodolfoSantosUK <santoseuro2018@gmail.com>
2020-11-08 17:11:52 -0300

    Mapeando as entidades

---
+++ spring5-recipe-app-using-setters-on-pojos/src/main/java/guru/springframework/domain/Pet.java
@@ -0,0 +1,57 @@
+package guru.springframework.domain;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table( name = "pets")
+public class Pet extends BaseEntity {
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "type_id")
+    private PetType petType;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private Owner owner;
+
+    @Column(name = "birthDate")
+    private LocalDate birthDate;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public PetType getPetType() {
+        return petType;
+    }
+
+    public void setPetType(PetType petType) {
+        this.petType = petType;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+}
