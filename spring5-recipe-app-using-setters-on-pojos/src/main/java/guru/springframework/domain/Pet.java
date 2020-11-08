153e546b96d4c3723a1273c3d5bff7d26b25ff6c
rodolfoSantosUK <santoseuro2018@gmail.com>
2020-11-08 18:27:54 -0300

    Mapeando as entidades

--- spring5-recipe-app-using-setters-on-pojos/src/main/java/guru/springframework/domain/Pet.java
+++ spring5-recipe-app-using-setters-on-pojos/src/main/java/guru/springframework/domain/Pet.java
@@ -3,9 +3,11 @@ package guru.springframework.domain;
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
-@Table( name = "pets")
+@Table(name = "pets")
 public class Pet extends BaseEntity {
 
     @Column(name = "name")
@@ -22,6 +24,8 @@ public class Pet extends BaseEntity {
     @Column(name = "birthDate")
     private LocalDate birthDate;
 
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     public String getName() {
         return name;
---
+++ spring5-recipe-app-using-setters-on-pojos/src/main/java/guru/springframework/domain/Speciality.java
@@ -0,0 +1,22 @@
+package guru.springframework.domain;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialities")
+public class Speciality extends BaseEntity {
+
+    @Column(name = "description")
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
